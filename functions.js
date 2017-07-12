b4af7aae03488768a8a91202e264414af4f221a2
onixks <darenicks365@gmail.com>
2017-07-12 11:10:08 -0400

    second commit

---
+++ functions.js
@@ -0,0 +1,16 @@
+//functions wellohorld(){
+   // return "wello horld";
+   // console.log ("wello horld");
+
+//}
+//functionName();
+
+function Rand(num){
+    //returns randon number between 0- num
+var randnum = Math.random()*num;
+var result Math.floor(randnum);
+return = result;
+
+}
+
+rand(60);
